1ce69a70a721d600222f4996f10141272a4136b7
Marcello-Cruz <marcellinhopastelzinho@gmail.com>
2022-06-24 21:55:14 -0300

    add color violet

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿console.writeline ("Achei um porta-retrato, cheio de foto, guardado" foreground color = ConsoleColor.Violet);
+console.writeline ("No fundo do meu armário e resolvi abrir" foreground color = ConsoleColor.Violet);
+console.writeline ("Um Polaroid manchado da gente junto abraçado" foreground color = ConsoleColor.Violet);
+console.writeline ("Sentado na cama vendo o sol subir" foreground color = ConsoleColor.Violet);
+console.writeline ("Teu sorriso brilhante no topo da roda-gigante" foreground color = ConsoleColor.Violet);
+console.writeline ("Segurando com cuidado a torta de chantilly" foreground color = ConsoleColor.Violet);
+console.writeline ("Meu olhar preocupado te olhando adiante" foreground color = ConsoleColor.Violet);
+console.writeline ("Perguntando se aquilo não ia cair" foreground color = ConsoleColor.Violet);
+
+console.readline ("");
+
+console.writeline ("Memórias são só memórias" foreground color = ConsoleColor.Violet);
+console.writeline ("Que fotos contem a história" foreground color = ConsoleColor.Violet);
+console.writeline ("Do que eu quis construir" foreground color = ConsoleColor.Violet);
+console.writeline ("Onde quer que tu esteja" foreground color = ConsoleColor.Violet);
+console.writeline ("Não importa o que eu cante" foreground color = ConsoleColor.Violet);
+console.writeline ("Porque você não vai mais me ouvir" foreground color = ConsoleColor.Violet);
+
+console.readline ("");
+
+console.writeline ("Tô correndo, mas tô sempre no lugar, yo" foreground color = ConsoleColor.Violet);
+console.writeline ("Um passo atrás de poder te alcançar, e eu vou" foreground color = ConsoleColor.Violet);
+console.writeline ("Tênis sujo e rasgado, de fugir da dor" foreground color = ConsoleColor.Violet);
+console.writeline ("Mas me espera, amor, mas me espera, amor" foreground color = ConsoleColor.Violet);
+console.writeline ("Tô correndo, mas tô sempre no lugar, yo" foreground color = ConsoleColor.Violet);
+console.writeline ("Um passo atrás de poder te alcançar, e eu vou" foreground color = ConsoleColor.Violet);
+console.writeline ("Tênis sujo e rasgado, de fugir da dor" foreground color = ConsoleColor.Violet);
+console.writeline ("Mas me espera, amor, mas me espera, amor" foreground color = ConsoleColor.Violet);
+
+console.readline ("");
+
+console.writeline ("Minha mãe me dizia, Filho, segue o ditado:" foreground color = ConsoleColor.Violet);
+console.writeline ("Quando é pra ser, será" foreground color = ConsoleColor.Violet);
+console.writeline ("Então, desesperado, eu olho pra todo lado" foreground color = ConsoleColor.Violet);
+console.writeline ("Mas eu não te vejo lá" foreground color = ConsoleColor.Violet);
+console.writeline ("Quando eu penso na vida, acho que eu tava errado" foreground color = ConsoleColor.Violet);
+console.writeline ("Mas demorei muito pra me ligar" foreground color = ConsoleColor.Violet);
+console.writeline ("Sinto o tempo parado, o que já foi é passado" foreground color = ConsoleColor.Violet);
+console.writeline ("E não dá mais pra eu voltar" foreground color = ConsoleColor.Violet);
+
+console.readline ("");
+
+console.writeline ("Memórias são só memórias" foreground color = ConsoleColor.Violet);
+console.writeline ("Que fotos contem a história" foreground color = ConsoleColor.Violet);
+console.writeline ("Do que eu quis construir" foreground color = ConsoleColor.Violet);
+console.writeline ("Onde quer que tu esteja" foreground color = ConsoleColor.Violet);
+console.writeline ("Não importa o que eu cante" foreground color = ConsoleColor.Violet);
+console.writeline ("Porque você não vai mais me ouvir" foreground color = ConsoleColor.Violet);
+console.writeline ("Tô correndo, mas tô sempre no lugar, yo" foreground color = ConsoleColor.Violet);
+console.writeline ("Um passo atrás de poder te alcançar, e eu vou" foreground color = ConsoleColor.Violet);
+console.writeline ("Tênis sujo e rasgado, de fugir da dor" foreground color = ConsoleColor.Violet);
+console.writeline ("Mas me espera, amor, mas me espera, amor" foreground color = ConsoleColor.Violet);
+console.writeline ("Tô correndo, mas tô sempre no lugar, yo" foreground color = ConsoleColor.Violet);
+console.writeline ("Um passo atrás de poder te alcançar, e eu vou" foreground color = ConsoleColor.Violet);
+console.writeline ("Tênis sujo e rasgado, de fugir da dor" foreground color = ConsoleColor.Violet);
+console.writeline ("Mas me espera, amor, mas me espera, amor" foreground color = ConsoleColor.Violet);
